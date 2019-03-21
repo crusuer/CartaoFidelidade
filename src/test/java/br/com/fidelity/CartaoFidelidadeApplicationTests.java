33e65e2af3606ead4cfccd2c52c4ae3466f00994
Robson <robson_mt@hotmail.com>
2019-03-20 23:53:14 -0300

    Initial commit

---
+++ src/test/java/br/com/fidelity/CartaoFidelidadeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fidelity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CartaoFidelidadeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
